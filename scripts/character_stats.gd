d72711fec7e5fbe793b09e1a2922b97cf9c921c9
Armen138 <armen138@pme.me>
2020-09-05 20:27:46 -0400

    Created 'offline' character sheet for Cyan

--- scripts/character_stats.gd
+++ scripts/character_stats.gd
@@ -16,6 +16,7 @@ export(int) var mov_range
 export(int) var heal
 export(Resource) var item_atk
 export(Resource) var item_def
+export(String, "default", "archer", "swordsman", "mage", "ai_archer", "ai_swordsman", "ai_mage", "hero", "antagonist", "old_man", "cyan") var portrait_override
 
 var abilities
 var control
