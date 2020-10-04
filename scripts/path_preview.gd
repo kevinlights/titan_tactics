2db2056c9d6681e0a34742a9d7790ad67535d232
Umbra Dev <umbra.dev@gmx.com>
2020-10-04 23:04:14 +1300

    modify path preview to parse 3d points

--- scripts/path_preview.gd
+++ scripts/path_preview.gd
@@ -6,7 +6,8 @@ export(Color) var bad_color
 
 func preview_path(path, allowed = true):
 	var world_path = PoolVector2Array()
-	for point in path:
+	for point_3d in path:
+		var point = Vector2(point_3d.x, point_3d.z)
 		world_path.append(point * Vector2(TT.cell_size, TT.cell_size))
 
 	$path.default_color = good_color if allowed else bad_color
