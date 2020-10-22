422a8c77b4f483b37bdd9415758ea727278794ae
Armen138 <armen138@pme.me>
2020-10-22 11:18:07 -0400

    fix 'tip' UI showing during enemy turn

--- scripts/tip.gd
+++ scripts/tip.gd
@@ -3,7 +3,7 @@ extends Control
 onready var world = get_parent().get_parent()
 
 func _process(delta):
-	if get_parent().active or world.current[world.current_turn].size() == 1 or world.num_done < 2:
+	if world.current_turn == TT.CONTROL.AI or get_parent().active or world.current[world.current_turn].size() == 1 or world.num_done < 2:
 		hide()
 	else:
 		show()
