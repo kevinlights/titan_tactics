88e2ec695c0c3d555a2460ed813ceaf417c8ff04
Armen138 <armen138@pme.me>
2020-10-31 14:13:29 -0400

    Fixed lvl1 dialogue portraits

--- scripts/tip.gd
+++ scripts/tip.gd
@@ -3,7 +3,13 @@ extends Control
 onready var world = get_parent().get_parent()
 
 func _process(delta):
-	if world.current_turn == TT.CONTROL.AI or get_parent().active or world.current[world.current_turn].size() == 1 or world.num_done < 2:
+	if world.current_turn == TT.CONTROL.AI or get_parent().active:
 		hide()
 	else:
 		show()
+	if world.current[world.current_turn].size() == 1 or world.num_done < 2:
+		$AnimatedSprite.hide()
+		$tip.hide()
+	else:
+		$AnimatedSprite.show()
+		$tip.show()
