0853560ae8ca69e65673187f63d8f6070e588765
Armen138 <armen138@pme.me>
2020-10-31 14:50:02 -0400

    Fix charswitch tip showing when appropriate

--- scripts/tip.gd
+++ scripts/tip.gd
@@ -2,12 +2,19 @@ extends Control
 
 onready var world = get_parent().get_parent()
 
+func get_characters_with_actions():
+	var num_done = 0
+	for character_check in world.current[TT.CONTROL.PLAYER]:
+		if character_check.is_done:
+			num_done += 1
+	return world.current[TT.CONTROL.PLAYER].size() - num_done
+
 func _process(delta):
 	if world.current_turn == TT.CONTROL.AI or get_parent().active:
 		hide()
 	else:
 		show()
-	if world.current[world.current_turn].size() == 1 or world.num_done < 2:
+	if world.current[TT.CONTROL.PLAYER].size() == 1 or get_characters_with_actions() < 2:
 		$AnimatedSprite.hide()
 		$tip.hide()
 	else:
