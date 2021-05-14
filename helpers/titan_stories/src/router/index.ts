aa55f643aa16ea3f372f0d6c149cb71d4257edcd
Armen <armen@nieuwenhuizen.ca>
2021-05-13 23:47:36 -0400

    ts style fixes

--- helpers/titan_stories/src/router/index.ts
+++ helpers/titan_stories/src/router/index.ts
@@ -1,30 +1,30 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-// import Home from '../views/Home.vue'
-import CutScene from '../views/CutScene.vue'
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+// import Home from "../views/Home.vue"
+import CutScene from "../views/CutScene.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'CutScene',
+    path: "/",
+    name: "CutScene",
     component: CutScene
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
