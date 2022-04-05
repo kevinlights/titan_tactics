68f3df4cd4a9fb9a2a7a0f131d9a3ccd84424c61
Armen <armen138.pm.me>
2022-04-04 21:43:30 -0400

    Adding localization to credits

--- scripts/gui/action_menu.gd
+++ scripts/gui/action_menu.gd
@@ -27,24 +27,6 @@ var label_map = {
 		TT.TYPE.ARCHER: [ "Sharp Shot", "Flame Shower", "Guard", "End" ],
 		TT.TYPE.FIGHTER: [ "Heavy Blow", "Sweeping Blow", "Guard", "End" ]
 }
-#	"nl-nl": {
-#		TT.TYPE.MAGE: [ "Bliksem Schicht", "Donder", "Helend Licht", "Beurt Beeindigen" ],
-#		TT.TYPE.ARCHER: [ "Scherp Schot", "Vlammen Regen", "Verdedigen", "End" ],
-#		TT.TYPE.FIGHTER: [ "Heavy Blow", "Sweeping Blow", "Guard", "End" ]		
-#	}
-#}
-
-#var tooltips = {
-#	"Heavy Blow": "A mighty swing of the sword that targets one",
-#	"Sweeping Blow": "Slash up to three enemies in front of you with this devastating attack",
-#	"Sharp Shot": "Line up an arrow and let it loose with frightening accuracy",
-#	"Flame Shower": "Shoots a bundle of flaming arrows that can damage up to 5 enemies at once. May cause burn",
-#	"Lightning Bolt": "High precision electric projectile that targets one",
-#	"Thunder Storm": "Summons a mighty storm that can damage up to 4 enemies at once. May stun enemies",
-#	"Healing Light": "Restores a little bit of health to yourself or an ally",
-#	"Guard": "Adopts a defensive stance in order to reduce incoming damage",
-#	"End": "Ends turn for this character"
-#}
 
 func _ready():
 	pass
